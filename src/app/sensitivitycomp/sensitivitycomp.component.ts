bd18f1fcd04bde29f9e02d0f7363c68a98d1c5ac
Nathan Picchiottino <nathanpicchiottino@gmail.com>
2022-01-04 10:51:34 +0100

    Add of files

---
+++ src/app/sensitivitycomp/sensitivitycomp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sensitivitycomp',
+  templateUrl: './sensitivitycomp.component.html',
+  styleUrls: ['./sensitivitycomp.component.css']
+})
+export class SensitivitycompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
