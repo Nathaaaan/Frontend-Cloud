1ed7cc7de563a3b9475a2deb5194745a36fa677a
Nathan Picchiottino <nathanpicchiottino@gmail.com>
2022-01-04 10:59:17 +0100

    sensitivity model

--- src/app/models/sensitivity.ts
+++ src/app/models/sensitivity.ts
@@ -1,2 +1,62 @@
 export class Sensitivity {
+
+  private _questionId: number;
+  private _gameName: string;
+  private _sensitivityValue: number;
+  private _resolutionValue: string;
+  private _playerName: string;
+
+
+  constructor(questionId: number, gameName: string, sensitivityValue: number, resolutionValue: string, playerName: string) {
+    this._questionId = questionId;
+    this._gameName = gameName;
+    this._sensitivityValue = sensitivityValue;
+    this._resolutionValue = resolutionValue;
+    this._playerName = playerName;
+  }
+
+
+  get questionId(): number {
+    return this._questionId;
+  }
+
+  set questionId(value: number) {
+    this._questionId = value;
+  }
+
+  get gameName(): string {
+    return this._gameName;
+  }
+
+  set gameName(value: string) {
+    this._gameName = value;
+  }
+
+  get sensitivityValue(): number {
+    return this._sensitivityValue;
+  }
+
+  set sensitivityValue(value: number) {
+    this._sensitivityValue = value;
+  }
+
+  get resolutionValue(): string {
+    return this._resolutionValue;
+  }
+
+  set resolutionValue(value: string) {
+    this._resolutionValue = value;
+  }
+
+  get playerName(): string {
+    return this._playerName;
+  }
+
+  set playerName(value: string) {
+    this._playerName = value;
+  }
+
+  toString(): string {
+    return this.questionId + this.resolutionValue + this.sensitivityValue + this.playerName;
+  }
 }
